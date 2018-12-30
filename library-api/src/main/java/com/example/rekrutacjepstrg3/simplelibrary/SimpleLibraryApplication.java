a45de22f6119e9a659bb026738ccd1eba776d139
Paweł Jaśkiewicz <jpawz@outlook.com>
2018-12-30 15:33:37 +0100

    initial commit

---
+++ library-api/src/main/java/com/example/rekrutacjepstrg3/simplelibrary/SimpleLibraryApplication.java
@@ -0,0 +1,14 @@
+package com.example.rekrutacjepstrg3.simplelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLibraryApplication.class, args);
+	}
+
+}
+
